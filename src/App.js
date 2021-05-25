0bb0b9c6e623a780267686ff4084eedbdd98964e
sampsonwang <sampsonwang@tencent.com>
2021-05-25 11:34:58 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>填古诗游戏</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
