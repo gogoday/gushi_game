0b0866131ab04133d98c7b59fe660dd1177aeb6e
sampsonwang <sampsonwang@tencent.com>
2021-05-26 12:40:48 +0800

    优化体验

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState, useRef } from 'react';
 import lodash from 'lodash';
 import './App.css';
 import { getCookie, setCookie } from './util';
+import classname from 'classnames';
 
 const userId = getCookie('user-id');
 if (!userId) {
@@ -11,10 +12,12 @@ if (!userId) {
 function App() {
   const [data, setData] = useState({"author":"元礎","paragraphs":["寺隔*潮去","採*過泉聲","林塘*半宿","風*夜深來"],"title":"逸句","wrongSort":["秋","藥","殘","雨"]});
   const [newQ, setNewQ] = useState(false);
+  const [showLoading, setShowLoading] = useState(false);
   let oriData = useRef({});
   let selectWord = useRef([]);
   useEffect(() => {
     async function fetchData() {
+      setShowLoading(true);
       const result = await fetch('https://hello-cloudbase-1gjrribi96ea328d-1251036730.ap-shanghai.app.tcloudbase.com/getwords', {
         method: 'post',
         body: JSON.stringify({
@@ -28,11 +31,14 @@ function App() {
       return result.json();
     }
     fetchData().then((res) => {
+      setShowLoading(false);
       const { ret, data } = res;
       if (ret === 0) {
         console.log(data);
         oriData.current = lodash.cloneDeep(data);
         setData(data);
+      } else {
+        alert('服务异常，请点击换一题目')
       }
     })
   }, [newQ])
@@ -67,8 +73,10 @@ function App() {
     const body = JSON.stringify({
       select: selectWord.current,
       userId: getCookie('user-id'),
+      create_time: oriData.current.create_time,
     })
     console.log(selectWord.current);
+    setShowLoading(true);
     fetch('https://hello-cloudbase-1gjrribi96ea328d-1251036730.ap-shanghai.app.tcloudbase.com/check', {
       method: 'post',
       body: JSON.stringify(body),
@@ -78,7 +86,17 @@ function App() {
     })
     .then(response => response.json())
     .then(data => {
+      setShowLoading(false);
       console.log(data);
+      const { ret } = data;
+      if (ret === 4) {
+        alert('答案错误，请重新选择')
+      } else if (ret === 0) {
+        alert('恭喜你，答案正确')
+        setNewQ(!newQ);
+      } else {
+        alert('系统繁忙，请重试')
+      }
     })
   }
   const getData = () => {
@@ -115,10 +133,16 @@ function App() {
         <div className="op">
           <button onClick={reset}>重新选择</button>
           <button onClick={submit}>提交答案</button>
-          <button onClick={getData}>换一提</button>
+          <button onClick={getData}>换一题</button>
         </div>
         
       </header>
+      <div className={classname({
+        "loading-bg": true,
+        "show-flex": showLoading,
+      })}>
+        <div className="loading">LOADING ....</div>
+      </div>
     </div>
   );
 }
